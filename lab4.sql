0efce089b7d7e011dc84f4f17faebad006690020
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-05-19 10:23:02 +0300

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -16,7 +16,7 @@ SELECT job_id, count(employee_id)
 FROM employees
 GROUP BY job_id;
 
-SELECT DISTINCT count(manager_id) AS "Number Of Manager"
+SELECT count(DISTINCT manager_id) AS "Number Of Manager"
 FROM employees;
 
 SELECT max(salary)-min(salary) AS DIFFERENCE
@@ -28,8 +28,9 @@ GROUP BY manager_id;
 
 SELECT manager_id, min(salary)
 FROM employees
+WHERE manager_id IS NOT null
 GROUP BY manager_id 
-HAVING min(salary) >= 6000 AND manager_id IS NOT null
+HAVING min(salary) >= 6000
 ORDER BY min(salary) DESC;
 
 SELECT manager_id, avg(salary)
